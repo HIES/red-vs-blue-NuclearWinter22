16c9961fe34245b32821d7aaed7db7f61331a6d6
NuclearWinter22 <wilsonja@bearsmail.org>
2017-12-07 12:12:18 -0500

    Working EmptyMap

--- EmptyMap.java
+++ EmptyMap.java
@@ -1,6 +1,4 @@
 import java.io.File;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class EmptyMap {
@@ -11,46 +9,27 @@ public class EmptyMap {
     public static void visualize(String fileName) throws Exception{
         File f = new File("inputs/" + fileName);
         Scanner inputObject = new Scanner(f);
-        inputObject.useDelimiter("   ");
-        String axisLine1 = inputObject.nextLine();
-        String axisLine2 = inputObject.nextLine();
-        String[] axisString1 = axisLine1.split("   ");
-        String[] axisString2 = axisLine2.split("   ");
-        String[] axisStrings = new String[4];
-        System.arraycopy(axisString1, 0, axisStrings, 0, 2);
-        System.arraycopy(axisString2,0,axisStrings,2,2);
-        double[] bounds = new double[axisStrings.length];
-        for(int i = 0; i < axisStrings.length; i++){
-            bounds[i] = Double.parseDouble(axisStrings[i]);
+        double[] bounds = new double[4];
+        for (int i = 0; i < 4; i += 2) {
+            bounds[i] = inputObject.nextDouble();
+            bounds[i + 1] = inputObject.nextDouble();
+            inputObject.nextLine();
         }
-        //double[] bounds = new double[4];
-        //for (int i = 0; i < 4; i++) bounds[i] = Double.parseDouble(inputObject.next());
-        int testInt1 = ((int)bounds[2])-((int)bounds[0]);
-        int testInt2 = ((((int)bounds[3])-((int)bounds[1])));
+        StdDraw.setCanvasSize((((int) bounds[0] - (int) bounds[2]) * (512)) / ((int) bounds[1] - (int) bounds[3]), 512);
         StdDraw.setXscale(bounds[0], bounds[2]);
         StdDraw.setYscale(bounds[1], bounds[3]);
         int regionsNum = Integer.parseInt(inputObject.nextLine());
             for (int i = 0; i < regionsNum; i++) {
                 for (int y = 0; y < 3; y++) inputObject.nextLine();
                 int pointsNum = Integer.parseInt(inputObject.nextLine());
-                Queue<Double[]> points = new LinkedList<>();
-                for (int j = 0; j < pointsNum; j++) {
-                    Double[] thisPoint = new Double[2];
-                    String[] pointString = inputObject.nextLine().split("   ");
-                    for (int x = 0; x < pointString.length; x++) {
-                        thisPoint[x] = Double.parseDouble(pointString[x]);
-                    }
-                    points.add(thisPoint);
-                    System.out.println("point added" + j);
+                double[] pointsX = new double[pointsNum];
+                double[] pointsY = new double[pointsNum];
+                for (int z = 0; z < pointsNum; z++) {
+                    pointsX[z] = inputObject.nextDouble();
+                    pointsY[z] = inputObject.nextDouble();
+                    inputObject.nextLine();
                 }
-                for (int y = 0; y < points.size(); y++) {
-                    Double[] point1 = points.remove();
-                    Double[] point2 = points.peek();
-                    StdDraw.line(point1[0], point1[1], point2[0], point2[1]);
-                    System.out.println("line drawn" + y);
-                    points.add(point1);
-                }
-
+                StdDraw.polygon(pointsX, pointsY);
             }
         inputObject.close();
         StdDraw.enableDoubleBuffering();
